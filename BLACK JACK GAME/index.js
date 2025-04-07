54278a8cad2eeb932339f322fd7828b9bc3e56da
ExtortionBaby <58045149+Oselu@users.noreply.github.com>
2025-04-07 19:06:09 +0100

    Add files via upload

--- BLACK JACK GAME/index.js
+++ BLACK JACK GAME/index.js
@@ -4,15 +4,23 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let topMessage = document.getElementById ("top-message")
+let totalSum = document.getElementById ("total-sum")
+let cardPicks = document.getElementById ("card-picks")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function  startGame (){
+    cardPicks.textContent = "Your card picks are:" + firstCard + " " + "and" + " "  + secondCard
+    totalSum.textContent = "Their Sum is =" + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+    topMessage.textContent = message
 }
 
-console.log(message)
